5124cbb2b6b821bbc228310f2f86023fd89bb8c8
patilarya76 <patilaryaanil07@gmail.com>
2025-03-26 10:30:53 +0530

    some changes

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,12 +1,7 @@
 // filepath: c:\Users\lenovo\Downloads\ChannelBlend\Frontend\vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['@emailjs/browser'],
-    },
-  },
-})
+});
